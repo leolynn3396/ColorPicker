5fbf5f672bd83d7c97cbb38a1705083ac21056ae
Ling Huang <linghuang@Lings-MacBook-Pro.local>
2024-01-30 01:15:52 -0800

    extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-const Color = (props) => {
-  return <div className={props.color}></div>  //can also use destructured syntax 
-}
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+
+const Color = ({color, A, B}) => { 
+  
+  var foo = color 
+
+  if(color === B){
+    foo = color + " selected"
+  }
+  
+  return (
+    <div onClick={() => A(color)} className= {foo}
+     ></div>
+
+  )
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">
-        <Color color="red" />
-        <Color color="blue" />
-        <Color color="green" />
+      <div id="colors-list"> 
+        <Color color="red" A={setSelectedColor} B={selectedColor} />
+        <Color color="blue" A={setSelectedColor} B={selectedColor} />
+        <Color color="green" A={setSelectedColor} B={selectedColor} />
       </div>
     </div>
   );
